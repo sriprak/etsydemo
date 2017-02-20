b70b992fc6fedc7ebc8a5e71546f9268660ea883
Sriprasad Kannan <sriprak@clemson.edu>
2017-02-19 23:27:35 -0500

    Image Resolution

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,4 @@
 class Listing < ApplicationRecord
-	has_attached_file :image, styles: { medium: "200x200", thumb: "100x100>" }, default_url: "default.jpg"
+	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
